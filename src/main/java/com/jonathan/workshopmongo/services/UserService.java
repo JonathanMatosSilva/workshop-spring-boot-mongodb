500100667fa25ea07a17f22b0e1b6c1ae292d718
Jonathan Matos Silva <jonathanmatos.jm71@gmail.com>
2023-08-13 15:20:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/jonathan/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.jonathan.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonathan.workshopmongo.domain.User;
+import com.jonathan.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
